9f1473c169aa849f5ed47745ea734162d69c12a6
Dmytro Riazanov <dmytro.riazanov@globallogic.com>
2017-10-06 15:44:47 -0700

    set imlicit wait to 10 sec and remove sleeps

--- DriverBuilder7zero.py
+++ DriverBuilder7zero.py
@@ -8,21 +8,18 @@ class DriverBuilderAndroid(object):
         "Setup for the test"
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '7.0'
+        desired_caps['platformVersion'] = '7.1.1'
         desired_caps['deviceName'] = 'Android Emulator'
         # Returns abs path relative to this file and not cwd
         desired_caps['app'] = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), 'apps/InstaSize_R9_Test_3_7_6_107_collage_crash_fix_1.apk'))
+            os.path.join(os.path.dirname(__file__), '/Users/ryaz/Downloads/Instasize_20172109_release_3.9.9_118_amazon.apk'))
         desired_caps['appPackage'] = 'com.jsdev.instasize'
         desired_caps['appActivity'] = '.activities.MainActivity'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver.implicitly_wait(10)
 
     def __init__(self):
-        self.driver, self.driverSetup()
+        self.setUp()
 
     def teardown(self):
         self.driver.quit()
-
-    def driverSetup(self):
-        driverSetup = DriverBuilderAndroid()
-        driverSetup.setUp()
--- Experiment.py
+++ Experiment.py
@@ -15,22 +15,10 @@ def _by_link_text():
 
 class InstasizeFilterTest(unittest.TestCase):
      "Class to run tests on exporting photos to Instagram"
-     def setUp(self):
-         "Setup for the test"
-         desired_caps = {}
-         desired_caps['platformName'] = 'Android'
-         desired_caps['platformVersion'] = '6.0.1'
-         desired_caps['deviceName'] = '05157df532e5e40e'
-         # Returns abs path relative to this file and not cwd
-         desired_caps['app'] = os.path.abspath(
-             os.path.join(os.path.dirname(__file__), '/Users/tyler/Desktop/Instasize_20172109_3.9.9_118_google.apk'))
-         desired_caps['appPackage'] = 'com.jsdev.instasize'
-         desired_caps['appActivity'] = '.activities.MainActivity'
-         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
      def test_filter_uploads(self):
-        sleep(4)
-        driver = self.driver
+        driver_builder = DriverBuilderAndroid()
+        driver = driver_builder.driver
 
         # taps on the + icon
         addPhoto = GridPage(driver)
@@ -39,12 +27,10 @@ class InstasizeFilterTest(unittest.TestCase):
         # Asserts collapseIcon is displayed
         collapseIcon = GridPage(driver)
         collapseIcon.collapseIconFind()
-        sleep(3)
 
         # taps on the native photos container
         tapPhotoContainer = GridPage(driver)
         tapPhotoContainer.photoContainers()
-        sleep(3)
 
         # Asserts allPhotosButton is displayed
         allPhotosButton = PhotoLibraryAsserts(driver)
@@ -53,7 +39,6 @@ class InstasizeFilterTest(unittest.TestCase):
         # taps on the top left photo
         tapTopLeftPhoto = GridPage(driver)
         tapTopLeftPhoto.topLeftPhoto()
-        sleep(10)
 
         # Searches for the Review Popup and dismisses it
         try:
@@ -73,21 +58,17 @@ class InstasizeFilterTest(unittest.TestCase):
         # taps on share button
         tapShareButton = EditorPage(driver)
         tapShareButton.sharebutton()
-        sleep(7)
 
         # Taps on Instagram icon
         tapInstagram = GridPage(driver)
         tapInstagram.instagramIcon()
-        sleep(10)
 
         # Searches for Instagram android popup on bottom of screen
         try:
             self.driver.find_element_by_id("icon").click()
         except NoSuchElementException:
             print("Not required on this version of android")
-        sleep(5)
         self.driver.back()
-        sleep(5)
 
         # Asserts the + button is displayed
         addPhoto = GridPage(driver)

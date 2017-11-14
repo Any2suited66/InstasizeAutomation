2f65693a7f46991c495fbf97394d418f60bd89da
Dmytro Riazanov <dmytro.riazanov@globallogic.com>
2017-11-14 13:23:09 -0800

    chore: use env vars to store path to apk and android version

--- DriverBuilder7zero.py
+++ DriverBuilder7zero.py
@@ -11,11 +11,11 @@ class DriverBuilderAndroid(object):
         "Setup for the test"
         desired_caps = {}
         desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '6.0.1'
+        desired_caps['platformVersion'] = os.environ['IS_ANDROID_VERSION']
         desired_caps['deviceName'] = '8802edf5'
         # Returns abs path relative to this file and not cwd
         desired_caps['app'] = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '/Users/tyler/Desktop/Instasize apk/Instasize_20171311_release_4.0.5_123_google.apk'))
+            os.path.join(os.path.dirname(__file__), os.environ['IS_ANDROID_APK_PATH']))
         desired_caps['appPackage'] = 'com.jsdev.instasize'
         desired_caps['appActivity'] = '.activities.MainActivity'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)

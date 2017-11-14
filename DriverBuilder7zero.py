1a2608fd21d78a36fe11b03b0043dfe33c9d6fe7
Tyler Hackett <thackett86181@gmail.com>
2017-11-14 12:36:32 -0800

    Nose update

--- DriverBuilder7zero.py
+++ DriverBuilder7zero.py
@@ -15,7 +15,7 @@ class DriverBuilderAndroid(object):
         desired_caps['deviceName'] = '8802edf5'
         # Returns abs path relative to this file and not cwd
         desired_caps['app'] = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), '/Users/tyler/Desktop/Instasize apk/Instasize_20170811_release_4.0.4_122_google.apk'))
+            os.path.join(os.path.dirname(__file__), '/Users/tyler/Desktop/Instasize apk/Instasize_20171311_release_4.0.5_123_google.apk'))
         desired_caps['appPackage'] = 'com.jsdev.instasize'
         desired_caps['appActivity'] = '.activities.MainActivity'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)

37b12374607eacc942e8815536296a7ba4c25610
Tyler <thackett86181@gmail.com>
2017-12-11 14:57:09 -0800

    Updated some test scripts and created collage test scripts for a two image collage with filters.

---
+++ GridPageTests/test_CameraAddPhoto.py
@@ -0,0 +1,85 @@
+import unittest
+
+from Asserts import PhotoLibraryAsserts
+from DriverBuilder7zero import DriverBuilderAndroid
+from PaidFiltersPage import PaidEditorPage
+from InstasizePages import GridPage
+from TryExcepts import TryExcepts
+from time import sleep
+from InstasizePages import EditorPage
+
+
+def _by_link_text():
+    pass
+
+
+class CameraTest(unittest.TestCase):
+    # Class to run tests on exporting photos to Instagram
+
+    def test_filter_uploads(self):
+        driver_builder = DriverBuilderAndroid()
+        driver = driver_builder.driver
+
+        # taps on the + icon
+        addPhoto = GridPage(driver)
+        addPhoto.addPhotoTap()
+
+        # Asserts collapseIcon is displayed
+        collapseIcon = GridPage(driver)
+        collapseIcon.collapseIconFind()
+
+        # taps on the camera icon
+        tapCamera = GridPage(driver)
+        tapCamera.tap_camera_icon()
+
+        # takes a picture
+        take_picture = GridPage(driver)
+        take_picture.take_photo()
+
+        # tap retry
+        tap_retry = GridPage(driver)
+        tap_retry.tap_retry_camera()
+
+        # takes a picture
+        take_picture.take_photo()
+
+        # taps on ok
+        tapCameraOkay = GridPage(driver)
+        tapCameraOkay.tap_camera_ok()
+
+        # taps on the filter
+        filters = PaidEditorPage(driver)
+        filters.aryaFilter()
+
+        # Asserts tvFilterLevel is displayed
+        tvFilterLevel = PhotoLibraryAsserts(driver)
+        tvFilterLevel.tvFilterLevel()
+
+        # taps on share button
+        tapShareButton = EditorPage(driver)
+        tapShareButton.sharebutton()
+
+        # Taps on Instagram icon
+        tapInstagram = GridPage(driver)
+        tapInstagram.instagramIcon()
+
+        # Searches for Instagram android popup on bottom of screen
+        instagramSystemPopup = TryExcepts(driver)
+        instagramSystemPopup.instagramSystemPopup()
+
+        sleep(5)
+        driver.back()
+
+        # Asserts the + button is displayed
+        addPhoto = GridPage(driver)
+        addPhoto.addPhotoFind()
+
+        # Tears down the test
+        quitTest = EditorPage(driver)
+        quitTest.driverQuit()
+
+
+# ---START OF SCRIPT
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(CameraTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)

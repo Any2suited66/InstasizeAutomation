c526d24bd4d74f7c3f8518ce9a8aac5ad9fcf335
Tyler <thackett86181@gmail.com>
2017-12-14 14:58:31 -0800

    Updated some test scripts and created collage test scripts for a two image collage with filters.

--- SettingsPage.py
+++ SettingsPage.py
@@ -1,4 +1,7 @@
 from appium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from time import sleep
+
 
 class SettingsPage(object):
 
@@ -6,7 +9,30 @@ class SettingsPage(object):
         self.driver = driver
 
     def SettingsButton(self):
-        self.driver.find_element_by_id('ibSettingsIcon').click()
+        # had to add this try/except to take care of the premium popup screen.
+        # if premium popup screen is removed please delete this try/except to reduce testing time
+        for _ in xrange(5):
+            try:
+                el = self.driver.find_element_by_id("btnGetStarted")
+                if el.is_displayed():
+                    el.click()
+                    break
+            except NoSuchElementException:
+                print "element not found, please check manually and to make sure element is still present"
+                break
+
+        for _ in xrange(5):
+            try:
+                sleep(3)
+                el = self.driver.find_element_by_id("btnSkip")
+                if el.is_displayed():
+                    el.click()
+                    break
+            except NoSuchElementException:
+                print "element not found, please check manually and to make sure element is still present"
+                break
+            finally:
+                self.driver.find_element_by_id('ibSettingsIcon').click()
 
     def HDExportButtonTap(self):
         self.driver.find_element_by_id('switchExportImageQuality').click()
